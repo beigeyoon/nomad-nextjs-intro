7414866c6af038cbe77eb94c9177e9624ee4240a
beigeyoon <beige.yoon@gmail.com>
2022-08-23 23:19:38 +0900

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
+
+export default function About() {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1 className="active">About us</h1>
+    </div>
+  );
+}
